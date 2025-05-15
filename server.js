f566660a1b4bd3a7cea17f70d55b5abc82d64fd3
choichangyeon <changyon99@naver.com>
2025-05-15 10:12:06 +0900

    body parser 적용

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
